876678db52a66e1fea7df27162e99f1b807acd40
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 14:11:19 +0700

    Update restart.py

--- bot/handlers/restart.py
+++ bot/handlers/restart.py
@@ -9,13 +9,6 @@ from os import execl
 from sys import executable
 from bot.handlers import fs_utils
 
-def sendMessage(text: str, client: Client, message: Message):
-    try:
-        return client.send_message(chat_id=message.chat.id,
-                            reply_to_message_id=message.message_id,
-                            text=text)
-    except Exception as e:
-        LOGGER.error(str(e))
 
 @Client.on_message(filters.command(COMMAND.RESTART))
 def restart(client: Client, message: Message):
