6d2c01bde236674bcaa85b0cecd98ae66be8a254
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 22:05:35 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,25 +12,29 @@ import pickle
 from os import execl, path, remove
 
 loop = asyncio.get_event_loop()
-app = Client(
-    "botkaca",
-    bot_token=CONFIG.BOT_TOKEN,
-    api_id=CONFIG.API_ID,
-    api_hash=CONFIG.API_HASH,
-    workers=100,
-    workdir=os_path_join(CONFIG.ROOT, CONFIG.WORKDIR),
-    plugins=dict(root="bot/handlers")
-)
-app.set_parse_mode("html")
-LOGGER.info("Bot Started!")
-#signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
-app.run()
-if __name__ == '__main__':
+
+def main():
     fs_utils.start_cleanup()
+    # Check if the bot is restarting
     if path.exists('restart.pickle'):
         with open('restart.pickle', 'rb') as status:
             restart_message = pickle.load(status)
         restart_message.edit_text("Restarted Successfully!")
         remove('restart.pickle')
-        signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
-        app.send_message("testing")
+
+        
+    app = Client(
+        "botkaca",
+        bot_token=CONFIG.BOT_TOKEN,
+        api_id=CONFIG.API_ID,
+        api_hash=CONFIG.API_HASH,
+        workers=100,
+        workdir=os_path_join(CONFIG.ROOT, CONFIG.WORKDIR),
+        plugins=dict(root="bot/handlers")
+    )
+    app.set_parse_mode("html")
+    LOGGER.info("Bot Started!")
+    signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
+    app.run()
+if __name__ == '__main__':
+    main()
