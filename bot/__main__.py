8804ef2ad2a90059456d1d1f4f83ad0f1556626d
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 11:28:02 +0700

    try

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,49 +4,64 @@ from pyrogram.types import Message
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from bot import CONFIG, COMMAND, LOCAL, LOGGER, STATUS
 from bot.handlers import *
+from bot.handlers.fs_utils import fs_utils
 import asyncio
+import shutil, psutil
+import signal
+import pickle
+from os import execl, path, remove
 
+loop = asyncio.get_event_loop()
+def main():
+    fs_utils.start_cleanup()
+    # Check if the bot is restarting
+    if path.exists('restart.pickle'):
+        with open('restart.pickle', 'rb') as status:
+            restart_message = pickle.load(status)
+        restart_message.edit_text("Restarted Successfully!")
+        remove('restart.pickle')
+    
 # Initialize bot
-Client.UPDATES_WORKERS = 100
-Client.DOWNLOAD_WORKERS = 100
-app = Client(
-    "botkaca",
-    bot_token=CONFIG.BOT_TOKEN,
-    api_id=CONFIG.API_ID,
-    api_hash=CONFIG.API_HASH,
-    workers=100,
-    workdir=os_path_join(CONFIG.ROOT, CONFIG.WORKDIR),
-    plugins=dict(root="bot/handlers")
-)
-app.set_parse_mode("html")
-
-# register /start handler
-app.add_handler(
-    MessageHandler(
-        start_message_handler.func,
-        filters=filters.command(COMMAND.START)
+    app = Client(
+        ":memory:",
+        bot_token=CONFIG.BOT_TOKEN,
+        api_id=CONFIG.API_ID,
+        api_hash=CONFIG.API_HASH,
+        workers=32,
+        workdir=os_path_join(CONFIG.ROOT, CONFIG.WORKDIR),
+        plugins=dict(root="bot/handlers")
     )
-)
-
-if CONFIG.BOT_PASSWORD:
-    # register /pass handler
+    app.UPDATES_WORKERS = 100
+    app.DOWNLOAD_WORKERS = 100
+    app.set_parse_mode("html")
+    LOGGER.info("Bot Started!")
+    # register /start handler
     app.add_handler(
         MessageHandler(
-            password_handler.func,
-            filters = filters.command(COMMAND.PASSWORD)
+            start_message_handler.func,
+            filters=filters.command(COMMAND.START)
         )
     )
 
-    # take action on unauthorized chat room
-    app.add_handler(
-        MessageHandler(
-            wrong_room_handler.func,
-            filters = lambda msg: not msg.chat.id in STATUS.CHAT_ID
+    if CONFIG.BOT_PASSWORD:
+        # register /pass handler
+        app.add_handler(
+            MessageHandler(
+                password_handler.func,
+                filters = filters.command(COMMAND.PASSWORD)
+            )
         )
-    )
 
+        # take action on unauthorized chat room
+        app.add_handler(
+            MessageHandler(
+                wrong_room_handler.func,
+                filters = lambda msg: not msg.chat.id in STATUS.CHAT_ID
+            )
+        )
+    loop.create_task(app.run())
 if __name__ == '__main__':
-    app.run()
+    main()
     #loop = asyncio.get_event_loop()
     #loop.create_task()
     #try:
