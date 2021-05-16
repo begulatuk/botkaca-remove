457487d31ae8814ec245c28dfc50dbbdf70fdcf5
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 11:56:44 +0700

    Update restart.py

--- bot/handlers/restart.py
+++ bot/handlers/restart.py
@@ -6,7 +6,7 @@ from pyrogram import (
 from pyrogram.types import Message
 from os import execl
 from sys import executable
-
+from bot.handlers import fs_utils
 
 @Client.on_message(filters.command(COMMAND.RESTART))
 async def func(client: Client, message: Message):
