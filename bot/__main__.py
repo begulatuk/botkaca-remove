837aa02db0418478663cf81e8058cbc55c96bfd8
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 22:27:11 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -22,17 +22,43 @@ def main():
         remove('restart.pickle')
         
     app = Client(
-        "botkaca",
+        ":memory:",
         bot_token=CONFIG.BOT_TOKEN,
         api_id=CONFIG.API_ID,
         api_hash=CONFIG.API_HASH,
-        workers=100,
+        workers=32,
         workdir=os_path_join(CONFIG.ROOT, CONFIG.WORKDIR),
         plugins=dict(root="bot/handlers")
     )
+    app.UPDATES_WORKERS = 100
+    app.DOWNLOAD_WORKERS = 100
     app.set_parse_mode("html")
     LOGGER.info("Bot Started!")
     signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
+    # register /start handler
+    app.add_handler(
+        MessageHandler(
+            start_message_handler.func,
+            filters=filters.command(COMMAND.START)
+        )
+    )
+
+    if CONFIG.BOT_PASSWORD:
+        # register /pass handler
+        app.add_handler(
+            MessageHandler(
+                password_handler.func,
+                filters = filters.command(COMMAND.PASSWORD)
+            )
+        )
+
+        # take action on unauthorized chat room
+        app.add_handler(
+            MessageHandler(
+                wrong_room_handler.func,
+                filters = lambda msg: not msg.chat.id in STATUS.CHAT_ID
+            )
+        )
     app.run()
     
 if __name__ == "__main__":
