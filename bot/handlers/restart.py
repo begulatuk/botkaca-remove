ace5704c2a4d995356676befde4c647b391ffc6c
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 13:36:02 +0700

    Update restart.py

--- bot/handlers/restart.py
+++ bot/handlers/restart.py
@@ -19,9 +19,11 @@ def sendMessage(text: str, client: Client, message: Message):
 
 @Client.on_message(filters.command(COMMAND.RESTART))
 async def restart(client: Client, message: Message):
-    restart_message = client.send_message(chat_id=message.chat.id,
-                                   reply_to_message_id=message.message_id,
-                                   text="Restarting, Please wait!")
+    restart_message = sendMessage(
+        "Restarting, Please wait!",
+        client,
+        message
+    )
     # Save restart message object in order to reply to it after restarting
     fs_utils.clean_all()
     with open('restart.pickle', 'wb') as status:
