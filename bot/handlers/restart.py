97cf6305f32431cd1b2c90c252722f7c9035300b
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 22:33:56 +0700

    Update restart.py

--- bot/handlers/restart.py
+++ bot/handlers/restart.py
@@ -17,7 +17,7 @@ def restart(client: Client, message: Message):
     #    client,
     #    message
     #)
-    reply = await message.reply_text("Restarting, Please wait!")
+    reply = message.reply_text("Restarting, Please wait!")
     # Save restart message object in order to reply to it after restarting
     fs_utils.clean_all()
     with open('restart.pickle', 'wb') as status:
