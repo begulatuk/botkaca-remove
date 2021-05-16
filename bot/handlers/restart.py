0d6e51164ba35b5623807a2c24c88b89b5c9cd28
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 13:35:17 +0700

    Update restart.py

--- bot/handlers/restart.py
+++ bot/handlers/restart.py
@@ -7,12 +7,21 @@ from pyrogram import (
 from pyrogram.types import Message
 from os import execl
 from sys import executable
-from bot.handlers import fs_utils
+
+
+def sendMessage(text: str, client: Client, message: Message):
+    try:
+        return client.send_message(chat_id=message.chat.id,
+                            reply_to_message_id=message.message_id,
+                            text=text)
+    except Exception as e:
+        LOGGER.error(str(e))
 
 @Client.on_message(filters.command(COMMAND.RESTART))
 async def restart(client: Client, message: Message):
-    restart_message = await message.reply_text(
-        "Restarting, Please wait!")
+    restart_message = client.send_message(chat_id=message.chat.id,
+                                   reply_to_message_id=message.message_id,
+                                   text="Restarting, Please wait!")
     # Save restart message object in order to reply to it after restarting
     fs_utils.clean_all()
     with open('restart.pickle', 'wb') as status:
