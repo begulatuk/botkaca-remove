ff275d1abf136a20b41bf21fa81d186faa83fb9c
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-16 22:59:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-# Install aria2c static binary
-wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
-tar xf aria2-1.35.0-static-linux-amd64.tar.gz
-export PATH=$PWD:$PATH
+wget -q https://kmk.kmk.workers.dev/aria2-1.35.0-static-linux-amd64.tar.gz
+tar xf aria2-1.35.0-static-linux-amd64.tar.gz -C /usr/local/bin
+chmod +x /usr/local/bin/aria2c
 rm -rf aria2-1.35.0-static-linux-amd64.tar.gz
 
 # Create download folder
@@ -14,8 +13,4 @@ rm -rf aria2-1.35.0-static-linux-amd64.tar.gz
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
-wget -q https://kmk.kmk.workers.dev/accounts.zip
-unzip -q accounts.zip
-rm -rf accounts.zip
 
-echo $PATH > PATH
