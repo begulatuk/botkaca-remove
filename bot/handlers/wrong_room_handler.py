a6b42d60fc894d1f23d975e291791bfefc016789
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-22 16:02:48 +0700

    Update wrong_room_handler.py

--- bot/handlers/wrong_room_handler.py
+++ bot/handlers/wrong_room_handler.py
@@ -1,12 +1,14 @@
 from pyrogram import Client
 from pyrogram.types import Message
-from bot import LOCAL, CONFIG
+from bot import LOCAL, CONFIG, STATUS
 
+@Client.on_message(~filters.chat(chats=STATUS.CHAT_ID))
 async def func(client : Client, message: Message):
-    chat_type = message.chat.type
     if message.chat.type != "private":
         await message.reply_text(
             LOCAL.WRONG_ROOM.format(
                 CHAT_ID = message.chat.id
             )
-        )    
+        )
+    else:
+        await message.delete()
