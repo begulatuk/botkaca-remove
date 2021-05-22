2bc5f8d35c2a34bd4db49c040666973ba64e8fae
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-22 11:22:15 +0700

    Update wrong_room_handler.py

--- bot/handlers/wrong_room_handler.py
+++ bot/handlers/wrong_room_handler.py
@@ -4,14 +4,11 @@ from bot import LOCAL, CONFIG
 
 async def func(client : Client, message: Message):
     if message.chat.type != "private":
-        try:
-            await message.reply_text(
-                LOCAL.WRONG_ROOM.format(
-                    CHAT_ID = message.from_user.id       
-                )
-            )    
-        except:
-            pass
-    else:
-        await message.delete(revoke=True)
+        await message.reply_text(
+            LOCAL.WRONG_ROOM.format(
+                CHAT_ID = message.from_user.id       
+            )
+        )
+        await client.leave_chat(chat_id=message.chat.id, delete=True)
+    await message.delete(revoke=True)    
 
