41f370a2a4811c875268c22bb95606f7c4c4a16d
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-05-22 12:12:54 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,8 +56,8 @@ def main():
         app.add_handler(
             MessageHandler(
                 wrong_room_handler.func,
-                filters = ~filters.chat(chats=lambda msg: not msg.chat.id in STATUS.CHAT_ID)
-                #filters=~filters.chat(chats=STATUS.CHAT_ID)
+                #filters = ~filters.chat(chats=lambda msg: not msg.chat.id in STATUS.CHAT_ID)
+                filters = ~filters.chat(chats=STATUS.CHAT_ID)
             )
         )
     app.run()
