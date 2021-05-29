06e5888fa5e4a7fda26bd74e9d1b8825a48fa81d
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-05-29 22:28:25 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -14,7 +14,7 @@ CONFIG = Config({
     'BOT_TOKEN' : None,
     'BOT_PASSWORD' : None,
     'CHAT_ID' : '',
-    'EDIT_SLEEP' : 3,
+    'EDIT_SLEEP' : 30,
     'UPLOAD_MAX_SIZE' : 2000 * 1024 * 1024,
     'UPLOAD_MIN_SIZE' : 100 * 1024,
     'UPLOAD_AS_DOC' : 0,
